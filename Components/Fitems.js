8518ce3983f46a5d281e5022c7161101152df19a
Eshwar B Padkantwar <ebpadkantwar97@gmail.com>
2024-01-26 13:50:37 +0530

    made changes to fashion items

---
+++ Components/Fitems.js
@@ -0,0 +1,29 @@
+import Casuallinedjacket from "../Assets/Fashion Deals/Casuallinedjacket.jpg";
+import AnarkaliKurta from "../Assets/Fashion Deals/AnarkaliKurta.jpg";
+import handbag from "../Assets/Fashion Deals/handbag.jpg";
+import allensolly from "../Assets/Fashion Items/allensolly.jpg"
+import arrowmen from "../Assets/Fashion Items/arrowmen.jpg"
+import Aureliawomenskurta from "../Assets/Fashion Items/Aureliawomenskurta.jpg"
+import bibawomensdress from "../Assets/Fashion Items/bibawomensdress.jpg"
+import Levismenjeans from "../Assets/Fashion Items/Levismenjeans.jpg"
+import Madamewomenjeans from "../Assets/Fashion Items/Madamewomenjeans.jpg"
+import madamewomensjegging from "../Assets/Fashion Items/madamewomensjegging.jpg"
+import Wranglarmensjeans from "../Assets/Fashion Items/Wranglarmensjeans.jpg"
+import womensshortkurti from "../Assets/Fashion Items/womensshortkurti.jpg"
+
+let filteredProducts = [
+    { name: "Men's Casual Jacket", price: 1179, image: Casuallinedjacket }, 
+    { name: "Men's Regular Fit Shirt", price: 808, image: allensolly },
+    { name: "Arrow Men's Slim Shirt", price: 1029, image: arrowmen },
+    { name: "Women's Short Kurti", price: 499, image: womensshortkurti },
+    { name: "BIBA Women's Dress Shirt", price: 2100, image: bibawomensdress }, 
+    { name: "Aurelia Women's Kurta", price: 1729, image: Aureliawomenskurta },
+    { name: "Levi's Men's Fit Jeans", price: 2000, image: Levismenjeans },
+    { name: "Madame Women Jeans", price: 1349, image: Madamewomenjeans },
+    { name: "Women's Anarkali Kurta", price: 749, image: AnarkaliKurta }, 
+    { name: "Madame Purple Jegging", price: 879, image: madamewomensjegging },
+    { name: "Men's Grey Jeans ", price: 1599, image: Wranglarmensjeans },
+    { name: "Women's Leather bag", price: 1496, image: handbag }, 
+  ]
+
+  export default filteredProducts
--- Pages/Products Pages/Fashion.jsx
+++ Pages/Products Pages/Fashion.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import Navbar from "../../Components/Navbar";
+import Fashionitems from "../../Components/Fashionitems";
 
 function Fashion(){
 
     return(
         <div>
-            Fashion
-
+            <Navbar/>
+            <Fashionitems/>
         </div>
     )
 }
